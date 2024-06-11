29bff580d5c65537c209373fd509d8fe0cb6fc88
JGSphaela <sample@email.com>
2024-06-12 08:43:56 +0900

    test sucess

--- tests/test_gpib.py
+++ tests/test_gpib.py
@@ -6,7 +6,7 @@ from src.gpib.gpib_communication import GPIBCommunication
 def test_gpib_communication():
     gpib_comm = GPIBCommunication()
     print(gpib_comm.rm.list_resources())
-    # gpib_comm.connect_device("GPIB0::1::INSTR")
+    gpib_comm.connect_device("GPIB0::17::INSTR")
     gpib_comm.send_command("*IDN?")
     response = gpib_comm.read_response()
     print(response)

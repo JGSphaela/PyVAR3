af8b78523ead7a9f2989d7a23cdccf4bf895b7ab
JGSphaela <39881472+JGSphaela@users.noreply.github.com>
2024-06-13 01:28:48 +0900

    rage quit

--- debug_tests/test_gpib_command.py
+++ debug_tests/test_gpib_command.py
@@ -12,25 +12,46 @@ def test_gpib_command():
     # Set output format
     gpib_command.set_output_format(1, 1)
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Enable timer
     gpib_command.time_stamp(enable=True)
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
     # Set filter
     gpib_command.set_filter(True)
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Set sample averaging
     gpib_command.set_averaging(10, 1)
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Set measurement mode
     gpib_command.set_measurement_mode(16, [1])
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
     # Set SMU measurement mode
     gpib_command.set_smu_mode([1], 1)
     gpib_command.set_smu_mode([2], 1)
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Set current measuring range
     gpib_command.current_measurement_range(channel=1, current_range=-19)
     gpib_command.current_measurement_range(channel=2, current_range=-19)
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
     # Set sweep delay time
     # gpib_command.sweep_delay(hold=0)
@@ -50,8 +71,14 @@ def test_gpib_command():
     # Test setting voltage sweep
     gpib_command.set_voltage_sweep(channel=1, mode=1, v_range=0, start=0.0, stop=1.0, step=0.5, icomp=0.01)
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Test forcing voltage
     gpib_command.force_voltage(channel=2, v_range=0, voltage=1.0, icomp=0.01)
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
     # Test setting measurement mode
     # gpib_command.set_measurement_mode(mode=1, channels=[1])
@@ -59,8 +86,14 @@ def test_gpib_command():
     # Reset timer count
     gpib_command.reset_time(channels=[1])
 
+    # Query error
+    gpib_command.query_error(mode=1)
+
     # Test triggering measurement
     gpib_command.trigger_measurement()
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
     # Wait for measurement complete
     print(gpib_command.wait_pending())
@@ -73,6 +106,9 @@ def test_gpib_command():
     # Read the return data
     # print(gpib_comm.read_response())
     print(gpib_comm.read_ascii())
+
+    # Query error
+    gpib_command.query_error(mode=1)
 
 
 if __name__ == "__main__":

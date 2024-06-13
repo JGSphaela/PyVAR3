7f7dfa5c6b077a74e0776f7d3519b14630f897ce
JGSphaela <sample@email.com>
2024-06-14 00:06:07 +0900

    testing

--- debug_tests/test_gpib_command.py
+++ debug_tests/test_gpib_command.py
@@ -7,7 +7,7 @@ from src.gpib.gpib_command import GPIBCommand
 def test_gpib_command():
     gpib_comm = GPIBCommunication()
     gpib_command = GPIBCommand()
-    gpib_comm.connect_device("GPIB0::{gpib_id}::INSTR")
+    gpib_comm.connect_device("GPIB0::17::INSTR")
 
     # Initialize connection
     gpib_command.init_connection(gpib_id=17)

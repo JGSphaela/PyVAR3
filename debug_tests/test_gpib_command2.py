2435160d7046daf8aed78df9b7c6423e875f822d
JGSphaela <sample@email.com>
2024-06-15 00:29:56 +0900

    testing,bug fix

--- debug_tests/test_gpib_command2.py
+++ debug_tests/test_gpib_command2.py
@@ -9,14 +9,14 @@ from src.tests.basic_test import BasicTest
 def test_gpib_command():
     basic_test = BasicTest()
 
-    basic_test.multichannel_sweep_voltage(17, 2, 1, 0, 0.0, 1.0,
+    print(basic_test.multichannel_sweep_voltage(17, 2, 1, 0, 0.0, 1.0,
                                           21, 0.01, None,
                                           1, 0, 1, None,
                                           None, None, 3,
                                           0, 1, None,
                                           None, None, 4,
                                           0, 1, None,
-                                          None, None,)
+                                          None, None,))
 
 
 if __name__ == "__main__":
